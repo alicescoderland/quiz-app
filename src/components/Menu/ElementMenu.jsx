cfd35af3bba17e02a715a484c7396d27d1df7ded
Alicja <a.wojcikbehar.code@gmail.com>
2023-09-29 10:04:49 +0200

    install react router dom

---
+++ src/components/Menu/ElementMenu.jsx
@@ -0,0 +1,15 @@
+
+function ElementMenu() {
+    return (
+      <section >
+       <div>
+        <div>img</div>
+        <p>level</p>
+        <p>Name</p>
+       </div>
+      </section>
+    );
+  }
+  
+  export default ElementMenu;
+  
---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,15 @@
+import ElementMenu from "./ElementMenu";
+
+function Menu() {
+  return (
+    <section>
+     <h1>Let's Try</h1>
+     <p>Test your knowledge</p>
+     <ElementMenu />
+     <ElementMenu />
+     <ElementMenu />
+    </section>
+  );
+}
+
+export default Menu;
