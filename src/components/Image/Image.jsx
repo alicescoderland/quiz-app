bbed63c08fa91878a4c84a1fbfadfb92e6809612
Alicja <a.wojcikbehar.code@gmail.com>
2023-10-26 16:28:23 +0200

    add media queries

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -2,7 +2,8 @@ import imgStyles from "./Image.module.css"
 
 function Image({img}) {
   return (
-    <section className={imgStyles.container} >
+    <section className={imgStyles.container}>
+      
       <img className={imgStyles.img} src={img} alt="Bgc of people" />
     </section>
   );
